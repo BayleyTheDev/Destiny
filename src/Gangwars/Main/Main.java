8a3b46baec3c870ce6ccc5553055b1cba47c8cf9
Bayley_LOL <bayleyfoster@hotmail.com>
2015-04-25 11:06:20 +0930

    Join event

--- src/Gangwars/Main/Main.java
+++ src/Gangwars/Main/Main.java
@@ -1,11 +1,36 @@
 package Gangwars.Main;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+
 
 /**
  * Created by Bayley on 4/24/2015.
  */
-public class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
     ///Hello
     //Test
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Minecraft Roleplay TTT has been launched.");
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("Minecraft Roleplay TTT registered events.");
+    }
+    @EventHandler
+    public void onLogin(PlayerJoinEvent event) {
+        event.setJoinMessage(ChatColor.YELLOW + event.getPlayer().getName() + ChatColor.RESET + " has connected to Minecraft Roleplay.");
+
+
+    }
+
+
 }
