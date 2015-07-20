c2c6bc47bf9010e3c640a6226144548065f6fa10
Bayley_LOL <bayleyfoster@hotmail.com>
2015-07-20 13:16:16 +0930

    Destiny renames.

---
+++ src/com/bayley/destiny/Destiny.java
@@ -0,0 +1,23 @@
+package com.bayley.destiny;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Bayley on 7/20/2015.
+ */
+public class Destiny extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+
+    }
+}
